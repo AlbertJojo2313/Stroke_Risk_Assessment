d338cb39f908841cf19e461d2c89099e5aaee7f4
Albert Jojo <jojoalbert35@gmail.com>
2025-02-15 15:24:50 -0500

    -m

--- code/preprocess.py
+++ code/preprocess.py
@@ -8,7 +8,7 @@ def open_files(file_path):
 
 
     """Loads CSV files into DataFrames with optimized dtypes."""
-    datasets = ["kaggle_stroke.csv", "brfss.csv"]
+    datasets = ["kaggle_stroke.csv", "brfss_2023.csv"]
     dfs = []
 
     for dataset in datasets:
@@ -36,11 +36,20 @@ def preprocess_data():
         kaggle_df.drop(columns=['id', 'Residence_type'], inplace=True, errors='ignore')
         kaggle_df = kaggle_df.astype({'age': 'int32', 'bmi': 'float32'}, errors='ignore')  # Optimize types
 
-
+    ##Brfss_cols
+    columns_to_keep = [
+        'HTM4', 'BMI', 'CVDSTRK3', 'BPHIGH6', 'BPMEDS1', 'TOLDHI3', 'CHOLMED3', 'CVDINFR4', 
+        'CVDCRHD4', 'DIABETE4', 'PREDIAB2', 'DIABTYPE', 'INSULIN1', '_RFHYPE6', '_RFCHOL3', '_MICHD',
+        '_SMOKER', 'SMOKE100', 'SMOKEDAY2', 'USENOW3', 'ECIGNOW2', '_CURECI2', 'STOPSMK2', 'MENTCIGS', 
+        'MENTECIG', 'ALCDAY4', 'AVEDRNK3', 'DRNK3GE5', 'MAXDRNKS', 'DRNKANY6', '_RFBING6', '_RFDRHV8',
+        '_TOTINDA', 'PA3MIN', 'PA3VIGM_', '_PACAT3', 'MARITAL', 'EMPLOY1', 'INCOME3', 'RENTHOM1',
+        'VETERAN3', 'LSATISFY', 'EMTSUPPRT', 'SDLONELY', 'SDHSTRE1', 'GENHLTH', 'PHYSHLTH', 'POORHLTH',
+        'COVIDPO1', 'COVIDVA1', 'COVIDNU2', 'MARIJANI', 'RRCLASS3', 'RRHCARE4', '_IMPRACE', '_RACEGR3',
+        '_RACEPRV', '_METSTAT', '_URBSTAT', 'MSCODE', '_HLTHPL1', '_HCVU653', 'LCSNUMCG', 'LCSCTSC1',
+        'LCSSCNCR', 'CRGVPRB3', 'ASPIRIN', '_LLCPWT'
+    ]
     if brfss_df is not None:
-        pass
-
-
+        brfss_df = brfss_df.filter(items=columns_to_keep)
     return kaggle_df, brfss_df
 
 def main():
